562f24cadd3341e945d29b42278e57cced6523d2
Ali <forsteve79@gmail.com>
2021-01-04 12:39:06 +0200

    Discovering GraphQL With Basic API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
-// Code to Test Before Starting The Actual project Coding
-(() => {
-    console.log("Hello From First Graphql Project (The Basics)");
-})();
+// Trying to Create The 1st/Most Basic GraphQL server/api
+const GraphQLServer = require('graphql-yoga').GraphQLServer;
+
+// 1 --> The Application typeDefs (Schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`;
+
+// 2 --> The Application Resolvers (Controllers in Rest APIs)
+const resolvers = {
+    Query: {
+        hello() {
+            return 'Hello World -- This is Our First Query From First Resolver';
+        },
+        name() {
+            return "My Name's Ali Fakhri and I'm Trying to Copy/Paste The Code From The Course"
+        },
+        location() {
+            return "I'm From Egypt"
+        },
+        bio() {
+            return "JS Developer Inshullah"
+        }
+    }
+};
+
+// 3 --> Now We Initiate The Server
+const graphQLServer = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+// 4 --> After Initiating The GraphQL Server WE Start The it
+graphQLServer.start(() => console.log('The GraphQL Server Started Successfully'));
